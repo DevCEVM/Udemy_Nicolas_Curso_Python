20d20b6cc2d0600829774c38d774696fba53577d
DevCEVM <ce.villegas92@gmail.com>
2025-12-22 21:32:55 -0600

    Create File

--- clases/04-propiedades-clase.py
+++ clases/04-propiedades-clase.py
@@ -1,4 +1,6 @@
 class MiPerro:
+    patas = 4
+
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
@@ -7,5 +9,11 @@ class MiPerro:
         print(f"{self.nombre} dice: Guau!")
 
 
+MiPerro.patas = 3
 mi_perro = MiPerro("Chanchito", 1)
-mi_perro.habla()
+mi_perro.patas = 5
+mi_perro2 = MiPerro("Felipe", 1)
+mi_perro2.patas = 8
+print(MiPerro.patas)
+print(mi_perro.patas)
+print(mi_perro2.patas)
